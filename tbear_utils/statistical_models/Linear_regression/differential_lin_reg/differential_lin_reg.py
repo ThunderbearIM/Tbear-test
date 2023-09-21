7a7e5d1caf62bfe383348117537703262d2d5cb2
Torbjorn <torbjorn@aralt.no>
2023-09-21 18:13:53 +0200

    Creating diff_lin_reg with github copilot as helper

--- tbear_utils/statistical_models/Linear_regression/differential_lin_reg/differential_lin_reg.py
+++ tbear_utils/statistical_models/Linear_regression/differential_lin_reg/differential_lin_reg.py
@@ -1,3 +1,4 @@
+import numpy
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -18,6 +19,10 @@ class DifferentialLinReg:
         Returns a sklearn Linear Regression model based on the values in the given Dataframe,
         where the first column is X and the second column is y
         If the dataframe is a timeseries, then the index is X, and the first column is y
+        
+        :param df: Dataframe with X and y values
+        :param ts: Boolean, if the dataframe is a timeseriese
+        :return: sklearn Linear Regression model
         """
         if ts:
             x = df.reset_index().iloc[:, 0].values()
@@ -30,4 +35,24 @@ class DifferentialLinReg:
         model = LinearRegression().fit(X=x,
                                        y=y)
         return model
+
+    @staticmethod
+    def diff_lin_reg_predict(self, model: LinearRegression, prediction_range: numpy.array(), last_datapoint: float):
+
+        """
+        Returns a prediction based on the given model and last datapoint
+        We add together each point of differentials to the last datapoint, where each addition is a new prediction step
+        Then transforms the prediction back from differentials to the original scale
+
+        :param model: sklearn Linear Regression model
+        :param prediction_range: range of predictions to make
+        :param last_datapoint:
+        :return: final_prediction
+        """
+
+        prediction = model.predict(prediction_range)
+        prediction= np.append(last_datapoint, prediction)
+        final_prediction = np.cumsum(prediction)
+
+        return final_prediction
 
