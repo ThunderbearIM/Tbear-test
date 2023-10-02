02f5a0aa071b6c78bb127cfcf450e97cbbc492fc
Torbjorn <torbjorn@aralt.no>
2023-10-02 12:44:24 +0200

    Pushing

--- tbear_utils/Evolutionary_learning/Dots/population.py
+++ tbear_utils/Evolutionary_learning/Dots/population.py
@@ -14,6 +14,7 @@ class Individual:
         self.time_left = time_left()
         self.velocity = velocity()
         self.accel = accel()
+        self.goal = np.array([5, 95])
 
     def genes(self):
         brain = self.brain
@@ -29,8 +30,67 @@ class Individual:
 
         return x
 
+    def distance_from_goal(self):
+
+    def fitness(self):
+        """
+        Evaluates the fitness of the dots based on distance from the goal in a 2d space with euclidian distance
+        :return: goodness_of_fit
+        """
+
+
+
+
+
+class Brain:
+    """
+    The brain of the dots individuals, we will
+    """
+
+    def __init__(self):
+        self.size=400
+        self.velocity = np.zeros([400, 2])
+        self.acceleration = self.acceleration()
+        self.position = np.array([[5, 5]])
+
+    def acceleration(self, size):
+        """
+        Acceleration method for the direction of the dots
+        :return:
+        """
+        x = np.random.uniform(low=-1, high=1, size=size)
+        y = np.random.uniform(low=-1, high=1, size=size)
+        accel = np.append(x, y).reshape(size, 2)
+        return accel
+
+    def velocity(self):
+        """
+        Takes the acceleration and adds it to the velocity
+        :return:
+        """
+        velocity = self.velocity
+        accel = self.acceleration
+
+        for i in range(1, len(velocity)):
+
+            velocity[i] = accel[i]+velocity[i-1]
+
+            if velocity[i][0] > 3:
+                velocity[i][0] = 3
+
+            if velocity[i][0] < -3:
+                velocity[i][0] = -3
+
+            if velocity[i][1] > 3:
+                velocity[i][1] = 3
+
+            if velocity[i][1] < -3:
+                velocity[i][1] = -3
+
+        return velocity
+
     def mutate(self):
-        genes = self.genes
+        genes = self.acceleration
         """
         Sees if the genes in the individual will mutate 
         :return: 
@@ -46,67 +106,17 @@ class Individual:
             mutate_y = np.random.uniform(low=0.9, high=1.1)
             genes = np.append(genes[0]*mutate_x, genes[1]*mutate_y)
 
-        self.genes = genes
-
-    def time_left(self, time = self.time_left):
-        if time:
-
-
-
-
-class Brain:
-    """
-    The brain of the dots individuals, we will
-    """
-
-    def __init__(self):
-        self.size=400
-        self.velocity = np.array([0, 0])
-        self.acceleration = self.acceleration()
-        self.position = np.array([0, 5])
-
-    @staticmethod
-    def acceleration():
-        """
-        Acceleration method for the direction of the dots
-        :return:
-        """
-        x = np.random.uniform(low=-1, high=1)
-        y = np.random.uniform(low=-1, high=1)
-        return x, y
-
-    def velocity(self):
-        """
-        Takes the acceleration and adds it to the velocity
-        :return:
-        """
-        velocity = self.velocity
-        x_accel, y_accel = self.acceleration
-        velocity = np.array([velocity[0]+x_accel, velocity[1]+y_accel])
-
-        if velocity[0] > 3:
-            velocity[0] = 3
-
-        if velocity[0] < -3:
-            velocity[0] = -3
-
-        if velocity[1] > 3:
-            velocity[1] = 3
-
-        if velocity[1] < -3:
-            velocity[1] = -3
-
-        self.velocity = velocity
-        return self.velocity
+        self.acceleration = genes
 
     def position(self):
         """
         Adds the current velocity to the position
         :return:
         """
-        pos = self.position
-        velocity = self.velocity()
-        pos[0] += self.velocity[0]
+        velocity = self.velocity
+        for i in range(len(velocity)):
+            new_pos = pos[i]+velocity[i]
+            pos = np.vstack((pos[i], new_pos))
 
 
 
@@ -114,3 +124,4 @@ class Population:
 
     def __init__(self):
         self.individual = Individual()
+        self.brain = Brain()
